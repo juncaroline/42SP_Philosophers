de2c764c1753e1d8d325b5f40cc36312e38f2a89
juncaroline <caroline_jun@hotmail.com>
2025-01-30 15:41:15 -0300

    philo

--- philo/threads.c
+++ philo/threads.c
@@ -6,38 +6,11 @@
 /*   By: cabo-ram <cabo-ram@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 11:41:18 by cabo-ram          #+#    #+#             */
-/*   Updated: 2025/01/30 11:47:31 by cabo-ram         ###   ########.fr       */
+/*   Updated: 2025/01/30 15:06:54 by cabo-ram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-/*int	create_thread(t_data *data, pthread_mutex_t *forks)
-{
-	pthread_t	manager;
-	int			i;
-
-	if (pthread_create(&manager, NULL, &monitor, data->philo_group) != 0)
-		destroy_all("Error creating monitor thread\n", data, forks);
-	i = 0;
-	while (i < data->philo_group[0].total_philos)
-	{
-		if (pthread_create(&data->philo_group[i].thread, NULL, &philo_routine,
-				&data->philo_group[i]) != 0)
-			destroy_all("Error creating philosopher thread\n", data, forks);
-		i++;
-	}
-	i = 0;
-	if (pthread_join(manager, NULL) != 0)
-		destroy_all("Error joining monitor thread\n", data, forks);
-	while (i < data->philo_group[0].total_philos)
-	{
-		if (pthread_join(data->philo_group[i].thread, NULL) != 0)
-			destroy_all("Error joining philosopher thread\n", data, forks);
-		i++;
-	}
-	return (0);
-}*/
 
 static int	create_thread(t_data *data, pthread_mutex_t *forks,
 	pthread_t *manager)
